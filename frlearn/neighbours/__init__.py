76ed85c321068abea622b56cd71257af6df07d67
Oliver Urs Lenz <oliver.urs.lenz@gmail.com>
2021-04-07 10:37:32 +0200

    make data descriptors available in base neighbours module

--- frlearn/neighbours/__init__.py
+++ frlearn/neighbours/__init__.py
@@ -2,6 +2,7 @@
 The :mod:`frlearn.neighbours` subpackage implements nearest neighbour algorithms.
 """
 
+from .descriptors import NND, LNND, LOF
 from .classifiers import FuzzyRoughEnsemble, FRNN, FRONEC, FROVOCO
 from .neighbour_search import BallTree, KDTree, NNSearch
 from .preprocessors import FRFS, FRPS
