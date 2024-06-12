acbb880fcd53cbfff272a2ffe16f7198b0ee06ed
kiffbe <71128817+kiffbe@users.noreply.github.com>
2024-06-12 11:26:01 +0100

    Update fuzzy_quantifiers.py

--- frlearn/uncategorised/fuzzy_quantifiers.py
+++ frlearn/uncategorised/fuzzy_quantifiers.py
@@ -1,6 +1,6 @@
 """
     Some fuzzy quantifiers.
-    All the following functions are the implementations of defintions of some fuzzy quantifiers in [1].
+    All the following functions are the implementations of definitions of some fuzzy quantifiers in [1].
 
     References
     ----------
@@ -8,28 +8,7 @@
     Fuzzy rough sets based on fuzzy quantification.
     Fuzzy Sets and Systems, 2023, vol. 473, p. 108704.
 """
-
-
-def choquet_integral(f, mu):
-    """
-        Implementation of the Choquet_integral
-        parameters:
-        ----------
-        f: list
-           Define a list that contains the values of the function
-        mu: list
-            Define a list that contains the values of the monotone measure.
-    """
-    n = len(mu)
-    f_sorted = sorted(f)
-    integral = 0
-    for i in range(n):
-        if i == 0:
-            integral = mu[0] * f_sorted[0]
-        else:
-            integral += mu[i] * (f_sorted[i] - f_sorted[i - 1])
-    return integral
-
+from frlearn.uncategorised import choquet_integral as ci
 
 def unary_zadeh_model(rim, A):
     """
@@ -55,50 +34,88 @@ def binary_zadeh_model(rim, A, B):
     A, B: table (list)
           List of value of the Fuzzy sets
     """
-    return rim(sum([el for el in A if el in B]) / sum(A))
+    return rim(sum([min(a, b) for a, b in zip(A, B)]) / sum(A))
 
 
-def unary_yager_model(A, mu):
+def unary_yager_model(rim, base_set, A):
     """
         Evaluation of unary sentences of the form “Λ X’s are A’s” in Yager’s OWA model, where Λ is a RIM quantifier
         parameters:
         ----------
         rim: function
             Define the RIM quantifier
+        base_set: list
+                  Define the base set on which the fuzzy set A is defined
         A: list
            Define the values of the fuzzy set A
-
-      mu: list
-         Define the list of values the monotone measure
         """
 
-    return choquet_integral(A, mu)
+    def mu_yager_unary(x):
+        return rim(len(x) / len(base_set))
+
+    set_v_sorted_1 = [i for _, i in sorted(zip(A, base_set))]
+    mu_yager_unary_val = []
+    for i in range(len(set_v_sorted_1)):
+        mu_yager_unary_val.append(mu_yager_unary(set_v_sorted_1[i:]))
+
+    return ci.choquet_integral(A, mu_yager_unary_val)
 
 
-def binary_yager_model(impl, mu):
-    """"
+def binary_yager_model(rim, impl, base_set, A, B=None):
+    """
       Evaluation of a binary sentence using Yager’s weighted implication-based binary quantification model
-      :parameter
+      parameters:
+      ----------
+      rim: function
+            Define the RIM quantifier
+
       impl: list
          Define the list of values of the Implicator of A and B
 
-      mu: list
-         Define the list of values of the monotone measure mu
-      """
+      base_set: list
+                  Define the base set on which the fuzzy set A is defined
+        A, B: list
+           Define the values of the fuzzy sets A and B
+    """
 
-    return choquet_integral(impl, mu)
+    def mu_yager_binary(x):
+        A_sorted = sorted(A)
+        sum_v = 0
+        for i in range(len(x)):
+            sum_v += A_sorted[i]
+        return rim(sum_v / sum(A))
+
+    set_v_sorted_2 = [i for _, i in sorted(zip(impl, base_set))]
+    mu_yager_binary_val = []
+    for i in range(len(set_v_sorted_2)):
+        mu_yager_binary_val.append(mu_yager_binary(set_v_sorted_2[i:]))
+
+    return ci.choquet_integral(impl, mu_yager_binary_val)
 
 
-def binary_WOWA_model(impl, mu):
+def binary_WOWA_model(rim, impl, base_set, A, B=None):
     """"
     Evaluation of a binary sentence using a fuzzy quantifier based
     on a Weighted Ordered Weighted Averaging (WOWA)
-    :parameter
-    impl: list
+    parameters:
+    ---------
+      rim: function
+            Define the RIM quantifier
+
+      impl: list
          Define the list of values of the Implicator of A and B
 
-      mu: list
-         Define the list of values of the monotone measure mu
+      base_set: list
+                  Define the base set on which the fuzzy set A is defined
+        A, B: list
+           Define the values of the fuzzy sets A and B
     """
 
-    return choquet_integral(impl, mu)
+    def mu_wowa(x):
+        return rim(sum([el for el in x if el in A]) / sum(A))
+
+    set_v_sorted_3 = [i for _, i in sorted(zip(impl, base_set))]
+    mu_wowa_val = []
+    for i in range(len(set_v_sorted_3)):
+        mu_wowa_val.append(mu_wowa(set_v_sorted_3[i:]))
+    return ci.choquet_integral(impl, mu_wowa_val)
